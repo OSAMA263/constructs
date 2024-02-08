ee3864bbba1403c0e1cfd158dbf500aa640dce1e
OSAMA263 <OSAMA263@users.noreply.github.com>
2024-02-08 03:50:09 +0000

    deploy: 03e220f3f4e424c587c066e5ae737f6a64546855

---
+++ assets/Services-c1KuvAW-.js
@@ -0,0 +1,13 @@
+import{j as e,P as a,L as n,S as m,h as x,O as o,p as d,F as p}from"./index-l9bTzal2.js";function j(){return e.jsxs(e.Fragment,{children:[e.jsx(a,{title:"Constructs - Services",description:"our blogs Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quaerat ipsa amet fuga ullam saepe?",href:"https://osama263.github.io/constructs/#/services",bg:"bg-gray-200",children:e.jsxs(n,{styles:"2xl:w-[60%]",children:[e.jsx(m,{h1:"our",h2:"services",par:"overview",styles:"flex-between border-black mb-20"}),e.jsx(x,{offsetY:"30%",children:e.jsx("div",{className:"space-y-10",children:u.map(t=>e.jsx(g,{data:t},t.title))})})]})}),e.jsx(o,{})]})}const g=({data:t})=>{const{lime_title:s,title:i,text:l,img:r,url:c}=t;return e.jsx("div",{className:"sticky top-0",children:e.jsxs("div",{className:"grid grid-cols-2 overflow-hidden rounded-2xl",children:[e.jsxs(h,{children:[e.jsx("h6",{className:"sm:text-base text-xs uppercase text-lime",children:s}),e.jsx("h1",{className:"xl:text-5xl sm:text-3xl text-2xl text-white",children:i}),e.jsx("p",{className:"sm:text-base text-xs text-white",children:l}),e.jsx(d,{to:c,className:"sm:p-4 sm:px-10 p-2 md:text-sm text-xs bg-lime hover:bg-lightGray",children:"LEARN MORE"})]}),e.jsx("img",{src:r,alt:i})]})})},h=p.div`
+flex 
+flex-col 
+items-center 
+justify-center 
+md:px-20 
+px-2 
+gap-y-4
+text-center 
+py-4 
+font-medium 
+bg-darkGray 
+`,u=[{lime_title:"home",title:"Residential",text:" Our skilled craftsmen specialize in residential construction.",img:"services/big-img1.webp",url:"residential"},{lime_title:"BUSINESS",title:"Commercial",text:"We are the ideal choice for commercial construction.",img:"services/big-img2.webp",url:"commercial"},{lime_title:"UPGRADE",title:"Renovation",text:"We specialize in breathing new life into existing spaces.",img:"services/big-img3.webp",url:"renovation"}];export{j as default};
