a80dd850d2b33a001ecf583c4691a6dafdb1a286
OSAMA263 <OSAMA263@users.noreply.github.com>
2024-02-28 11:09:37 +0000

    deploy: 24d45fd4429a197d8c77cdd1244248227481b35a

---
+++ assets/Projects-d42Wx-vy.js
@@ -0,0 +1,16 @@
+import{j as t,P as a,L as l,S as c,q as x,h as r,s as d,F as m}from"./index-WJgz7w7F.js";import{ContactForm as n}from"./Contact-ZyraKpLg.js";function f(){return t.jsxs(a,{title:"Constructs - Projects",description:"our projects Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quaerat ipsa amet fuga ullam saepe?",href:"https://osama263.github.io/constructs/#/projects",bg:"bg-black",children:[t.jsxs(l,{children:[t.jsx(c,{h1:"our",h2:"projects",par:"latest",styles:"border-lime mb-10 pb-10 sm:text-center sm:flex-col [&_p]:text-lightGray [&_h3]:text-lightGray [&_h2]:text-lime [&_h1]:text-white"}),t.jsx("div",{className:"grid md:grid-cols-2 py-10 gap-14",children:x.map((e,s)=>t.jsx(r,{styles:"top-0 sticky",ind:s+1,offsetY:"35%",children:t.jsx(h,{data:e})},e.title))})]}),t.jsx(d,{}),t.jsx("div",{className:"bg-gray-200",children:t.jsx(l,{id:"form",styles:"xl:w-[35%] lg:w-[40%] md:w-[50%] sm:w-[60%]",children:t.jsx(r,{offsetY:"35%",children:t.jsx(n,{title:"Request A Quote"})})})})]})}const h=({data:e})=>{const{title:s,img:i,text:o}=e;return t.jsxs("div",{className:"rounded-2xl overflow-hidden [&_img]:hover:scale-110 ",children:[t.jsx("img",{src:i,alt:s}),t.jsxs(p,{children:[t.jsx("h1",{className:"2xl:text-5xl xl:text-3xl text-xl font-bold",children:s}),t.jsx("p",{className:"text-lightGray xl:text-lg text-sm font-medium",children:o})]})]})},p=m.div`
+absolute
+w-full
+h-full
+bottom-0
+justify-end
+flex 
+flex-col
+bg-gradient-to-b
+from-[#b5b5b50a]
+to-[#292a2cf3]
+sm:py-10
+pb-1
+text-center
+space-y-2 rounded-2xl
+text-white`;export{f as default};
