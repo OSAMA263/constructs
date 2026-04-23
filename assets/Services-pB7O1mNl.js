e65fe51ab0d5b3bf3789fbff00cf9a714c2bc392
OSAMA263 <OSAMA263@users.noreply.github.com>
2026-04-23 04:36:08 +0000

    deploy: 9c7dc2d625dccc21fbfebfd06e0168135823a523

---
+++ assets/Services-pB7O1mNl.js
@@ -0,0 +1,13 @@
+import{j as e,P as a,L as n,S as o,h as m,O as x,p as d,F as p}from"./index-X2JpLczD.js";function j(){return e.jsxs(e.Fragment,{children:[e.jsx(a,{title:"Constructs - Services",description:"our blogs Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quaerat ipsa amet fuga ullam saepe?",href:"https://osama263.github.io/constructs/#/services",bg:"bg-gray-200",children:e.jsxs(n,{styles:"2xl:w-[60%]",children:[e.jsx(o,{h1:"our",h2:"services",par:"overview",des:"We deliver end-to-end construction solutions built on precision.",styles:"flex-between border-black mb-20"}),e.jsx(m,{offsetY:"30%",children:e.jsx("div",{className:"space-y-10",children:g.map(t=>e.jsx(u,{data:t},t.title))})})]})}),e.jsx(x,{})]})}const u=({data:t})=>{const{lime_title:s,title:i,text:l,img:r,url:c}=t;return e.jsx("div",{className:"sticky top-0",children:e.jsxs("div",{className:"grid grid-cols-2 overflow-hidden rounded-2xl",children:[e.jsxs(h,{children:[e.jsx("h6",{className:"sm:text-base text-xs uppercase text-lime",children:s}),e.jsx("h1",{className:"xl:text-5xl sm:text-3xl text-xl text-white",children:i}),e.jsx("p",{className:"sm:text-base max-sm:font-normal text-xs text-white",children:l}),e.jsx(d,{to:c,className:"sm:p-4 sm:px-10 p-[1px] md:text-sm text-xs bg-lime hover:bg-lightGray",children:"LEARN MORE"})]}),e.jsx("img",{src:r,className:"h-full ",alt:i})]})})},h=p.div`
+flex 
+flex-col 
+items-center 
+justify-center 
+md:px-20 
+px-2 
+gap-y-4
+text-center 
+py-8
+font-medium 
+bg-darkGray 
+`,g=[{lime_title:"home",title:"Residential",text:" Our skilled craftsmen specialize in residential construction.",img:"services/big-img1.webp",url:"residential"},{lime_title:"BUSINESS",title:"Commercial",text:"We are the ideal choice for commercial construction.",img:"services/big-img2.webp",url:"commercial"},{lime_title:"UPGRADE",title:"Renovation",text:"We specialize in breathing new life into existing spaces.",img:"services/big-img3.webp",url:"renovation"}];export{j as default};
